fe0dc65ff64e8ed998330ea91a8d9bb2bd0bb0c0
David Vazquez <dave_u@outlook.com>
2024-05-16 20:34:01 -0600

    rutas

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate( "/login", {
+            replace: true
+        });
+    }
+
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
             <Link 
                 className="navbar-brand" 
@@ -31,14 +40,18 @@ export const Navbar = () => {
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        className={ ({isActive}) => `nav-item nav-link ${ isActive ? "active" : "" }` }
-                        to="/login"
+                    <span className='nav-item nav-link text-primary'>
+                        Carlos
+                    </span>
+
+                    <button
+                        className='nav-item nav-link btn'
+                        onClick={onLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
